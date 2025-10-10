8a82fe351ab375dbe08c244a45b28286df3757c2
Martial Grin <49276099+martialgrin@users.noreply.github.com>
2025-10-10 15:24:13 +0200

    adding filter sorting starting point exercice

--- appendix_Manipulating_DOM/js/pages/projects.js
+++ appendix_Manipulating_DOM/js/pages/projects.js
@@ -2,20 +2,50 @@ import { fetchContent } from "../lib/api.js";
 
 async function main() {
 	const content = await fetchContent("./../assets/data/", "content.json");
-	const parentContainer = document.getElementById("projects-container");
+	const projectsContainer = document.getElementById("projects-container");
+	const tagsArray = [];
 	for (let i = 0; i < content.projects.length; i++) {
 		const container = document.createElement("div");
 		container.classList.add("project-container");
-
-		const title = createTitle(i);
 		const image = createImage(i);
-		console.log(title);
-		console.log(image);
-
-		container.appendChild(title);
+		getAllTags(content.projects[i].tags);
 		container.appendChild(image);
-		parentContainer.appendChild(container);
+		projectsContainer.appendChild(container);
 	}
+
+
+	const parentTagContainer = document.getElementById("tags-container");
+
+	for (let i = 0; i < tagsArray.length; i++) {
+		const tagButton = generateTagButton(tagsArray[i]);
+		parentTagContainer.appendChild(tagButton);
+	}
+
+	function getAllTags(tags) {
+		for (let i = 0; i < tags.length; i++) {
+			if (!tagsArray.includes(tags[i])) {
+				tagsArray.push(tags[i]);
+			}
+		}
+	}
+
+	function generateTagButton(content) {
+		const button = document.createElement("button");
+		button.innerText = content;
+		button.classList.add("tag");
+		button.addEventListener("click", () => {
+			onTagClickHandler(content, button);
+		});
+		return button;
+	}
+
+	function onTagClickHandler(tag, button) {
+		// Filter all projects
+		console.log(tag, button);
+		projectsContainer.innerHTML = "";
+	}
+
+	function createProject(project) {}
 
 	function createTitle(i) {
 		const titleElement = document.createElement("h2");
