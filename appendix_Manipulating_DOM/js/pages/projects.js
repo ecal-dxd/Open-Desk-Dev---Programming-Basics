f46769bf56b5a3266c9775dbd0089711462694fd
Martial Grin <49276099+martialgrin@users.noreply.github.com>
2025-10-10 15:51:01 +0200

    update the filter tags result

--- appendix_Manipulating_DOM/js/pages/projects.js
+++ appendix_Manipulating_DOM/js/pages/projects.js
@@ -5,14 +5,9 @@ async function main() {
 	const projectsContainer = document.getElementById("projects-container");
 	const tagsArray = [];
 	for (let i = 0; i < content.projects.length; i++) {
-		const container = document.createElement("div");
-		container.classList.add("project-container");
-		const image = createImage(i);
+		createProject(content.projects[i]);
 		getAllTags(content.projects[i].tags);
-		container.appendChild(image);
-		projectsContainer.appendChild(container);
 	}
-
 
 	const parentTagContainer = document.getElementById("tags-container");
 
@@ -41,24 +36,63 @@ async function main() {
 
 	function onTagClickHandler(tag, button) {
 		// Filter all projects
-		console.log(tag, button);
 		projectsContainer.innerHTML = "";
+		for (let i = 0; i < content.projects.length; i++) {
+			if (content.projects[i].tags.includes(tag)) {
+				createProject(content.projects[i]);
+			}
+		}
 	}
 
-	function createProject(project) {}
+	function createProject(p) {
+		const container = document.createElement("div");
+		container.classList.add("project-container");
+		const image = imageTags(p);
+		container.appendChild(image);
+		projectsContainer.appendChild(container);
+	}
 
-	function createTitle(i) {
+	function createTitle(title) {
+		const container = document.createElement("div");
+		container.classList.add("title-container");
 		const titleElement = document.createElement("h2");
-		titleElement.innerText = content.projects[i].title;
-		return titleElement;
+		titleElement.innerText = title;
+		container.appendChild(titleElement);
+		return container;
 	}
-	function createImage(i) {
-		const imageElement = document.createElement("img");
-		const path = "./../assets/images/";
-		imageElement.classList.add("thumb");
-		const src = content.projects[i].images[0] + ".jpeg";
-		imageElement.src = path + src;
-		return imageElement;
+
+	function imageTags(project) {
+		const container = document.createElement("div");
+		container.classList.add("absolute-image");
+		container.style.backgroundImage =
+			"url('../assets/images/" + project.images[0] + ".jpeg')";
+		const tags = createTags(project.tags);
+		const title = createTitle(project.title);
+		container.appendChild(title);
+		container.appendChild(tags);
+		return container;
+	}
+
+	function createTags(content, limit = content.length) {
+		const tagArray = [];
+		const container = document.createElement("div");
+		container.classList.add("tags-container");
+		for (let i = 0; i < limit; i++) {
+			const tag = createTag(content[i]);
+			tagArray.push(tag);
+			container.appendChild(tag);
+		}
+		return container;
+	}
+
+	function createTag(content) {
+		const container = document.createElement("div");
+		container.classList.add("tag-container");
+		const text = document.createElement("p");
+		text.classList.add("tag-text");
+		text.innerText = content;
+		container.appendChild(text);
+		return container;
 	}
 }
 
